b523c3f386e95e2e17f36d93865263834c129eb4
Kevin R. Rogala <krrogue@gmail.com>
2014-09-06 17:12:37 -0400

    Finish admin and delete

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -15,6 +15,15 @@ describe User do
   it { should respond_to(:authenticate) }
   it { should be_valid }
 
+  describe "with admin attribute set to 'true'" do
+    before do
+      @user.save!
+      @user.toggle!(:admin)
+    end
+
+    it { should be_admin }
+  end
+
   describe "when name is not present" do
   	before { @user.name = " " }
   	it { should_not be_valid }
